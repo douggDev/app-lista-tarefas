f4511d77ef2484323b1356c7f6e89baab21cd558
Douglas Pedro <douglas_silvapedro@hotmail.com>
2022-07-27 20:01:24 -0300

    criando funcão listar tarefas

--- app_lista_tarefas/tarefa_controller.php
+++ app_lista_tarefas/tarefa_controller.php
@@ -4,20 +4,27 @@
 	require "../../app_lista_tarefas/tarefa.service.php";
 	require "../../app_lista_tarefas/conexao.php";
 
-	echo '<pre>';
-	print_r($_POST);
-	echo '</pre>';
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-	$tarefa = new Tarefa();
-	$tarefa->__set('tarefa', $_POST['tarefa']);
+	if($acao == 'inserir'){
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
 
-	$conexao = new Conexao();
+		$conexao = new Conexao();
 
-	$tarefaService = new TarefaService($conexao, $tarefa);
-	$tarefaService->create();
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->create();
 
-	echo '<pre>';
-	print_r($tarefaService);
-	echo '</pre>';
+		header('Location: nova_tarefa.php?inclusao=1');
+	} else if($acao == 'recuperar'){
+		
+		$tarefa = new Tarefa();
+		$conexao = new Conexao();
+		
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->read();
+	}
+
+
 
 ?>
